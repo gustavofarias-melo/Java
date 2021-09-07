c7e8256d679acf8d5b6b6a5b4e2df065a5046d6d
Gustavo Farias <62080181+gustavofarias-melo@users.noreply.github.com>
2021-09-07 18:37:29 -0300

    Update Exercicio Udemy - Descobrir a área.java

--- Exercicio Udemy - Descobrir a área.java
+++ Exercicio Udemy - Descobrir a área.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		/*Exercicios 01*/
+
 		Locale.setDefault(Locale.US);
 		
 		Scanner sc = new Scanner(System.in);
